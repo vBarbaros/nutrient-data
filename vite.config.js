12b523d0adeddc14abb541d9c5b05ce610890b23
vBarbaros <victor.barbarosh@gmail.com>
2025-11-25 17:34:20 -0500

    Prepare new build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/nutrient-data/' : '/',
+  base: '/nutrient-data/',
   build: {
     outDir: 'docs'
   }
